c611b738bbe263f28850343f1977d06b68dba7e6
joaovictordejesus <166874133+joaovictordejesus@users.noreply.github.com>
2025-01-07 15:26:03 -0300

    Create AutorTest.java

---
+++ AutorTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AutorTest {
+    @Test
+    public void testGetNome() {
+        Autor autor = new Autor("Jess", "Brasileira");
+        assertEquals("Jess", autor.getNome());
+    }
+
+    @Test
+    public void testSetNome() {
+        Autor autor = new Autor("Jess", "Brasileira");
+        autor.setNome("Jessica");
+        assertEquals("Jessica", autor.getNome());
+    }
+}
